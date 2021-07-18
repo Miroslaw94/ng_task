198a1c4e66ecba8ba9fb2f99fb7ea1bc589da1a0
Miroslaw94 <miras_94@wp.pl>
2021-07-18 15:22:11 +0200

    Added Procfile for heroku. Added gunicorn to requirements. Changed app debug setting.

--- ng_task/ng_task/settings.py
+++ ng_task/ng_task/settings.py
@@ -24,9 +24,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    "ngtask.herokuapp.com"
+]
 
 
 # Application definition
